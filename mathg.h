3bef4f2922d7c6540e0eebf30979054847f33f0c
ignite720 <ignite720@outlook.com>
2025-01-15 19:04:41 +0800

    init2

--- mathg.h
+++ mathg.h
@@ -3,44 +3,59 @@
 #define GLM_FORCE_CTOR_INIT
 #define GLM_FORCE_EXPLICIT_CTOR
 #define GLM_FORCE_INLINE
-#define GLM_FORCE_XYZW_ONLY
+
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #define GLM_FORCE_LEFT_HANDED
 //#define GLM_FORCE_RIGHT_HANDED
-//#define GLM_FORCE_INTRINSICS
+
+#define GLM_FORCE_INTRINSICS
+
 //#define GLM_FORCE_CXX98
 //#define GLM_FORCE_CXX17
-#define GLM_ENABLE_EXPERIMENTAL
 //#define GLM_FORCE_MESSAGES
+#define GLM_ENABLE_EXPERIMENTAL
 
 #ifdef GLM_FORCE_INTRINSICS
-#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+    #undef GLM_FORCE_XYZW_ONLY
+    #define GLM_FORCE_ALIGNED
+#else
+    #define GLM_FORCE_PURE
 #endif
 
 #include <glm/glm.hpp>
-#include <glm/ext.hpp> // ext, gtc, gtx, etc.
+#include <glm/ext.hpp>          // ext, gtc, gtx, etc.
 
-using Vector2 = glm::vec2;
-using Vector3 = glm::vec3;
-using Vector4 = glm::vec4;
+#define DEFINE_EXTRA_VECTOR_TYPES(prefix, prefix_og) \
+using Vector2##prefix = glm::##prefix_og##vec2; \
+using Vector3##prefix = glm::##prefix_og##vec3; \
+using Vector4##prefix = glm::##prefix_og##vec4;
 
-using Vector3b = glm::bvec3;
+#define DEFINE_EXTRA_MATRIX_TYPES(prefix, prefix_og) \
+using prefix##2 = glm::##prefix_og##vec2; \
+using prefix##2x2 = glm::##prefix_og##mat2x2; \
+using prefix##2x3 = glm::##prefix_og##mat2x3; \
+using prefix##2x4 = glm::##prefix_og##mat2x4; \
+using prefix##3 = glm::##prefix_og##vec3; \
+using prefix##3x2 = glm::##prefix_og##mat3x2; \
+using prefix##3x3 = glm::##prefix_og##mat3x3; \
+using prefix##3x4 = glm::##prefix_og##mat3x4; \
+using prefix##4 = glm::##prefix_og##vec4; \
+using prefix##4x2 = glm::##prefix_og##mat4x2; \
+using prefix##4x3 = glm::##prefix_og##mat4x3; \
+using prefix##4x4 = glm::##prefix_og##mat4x4;
 
-using Vector2i = glm::ivec2;
-using Vector3i = glm::ivec3;
-using Vector4i = glm::ivec4;
+DEFINE_EXTRA_VECTOR_TYPES(, )
+DEFINE_EXTRA_VECTOR_TYPES(b, b)
+DEFINE_EXTRA_VECTOR_TYPES(i, i)
+DEFINE_EXTRA_VECTOR_TYPES(u, u)
+DEFINE_EXTRA_VECTOR_TYPES(u8, u8)
 
-using Vector2u = glm::uvec2;
-using Vector3u = glm::uvec3;
-using Vector4u = glm::uvec4;
+DEFINE_EXTRA_MATRIX_TYPES(Float, )
+DEFINE_EXTRA_MATRIX_TYPES(Sint, i)
+DEFINE_EXTRA_MATRIX_TYPES(Uint, u)
 
-using Vector2u8 = glm::u8vec2;
-using Vector3u8 = glm::u8vec3;
-using Vector4u8 = glm::u8vec4;
-
+using Matrix4x4 = Float4x4;
 using Quaternion = glm::quat;
-using Matrix3x3 = glm::mat3x3;
-using Matrix4x4 = glm::mat4x4;
 
 namespace mg = glm;
