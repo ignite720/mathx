93d1a0df59dd725851bb47229242ec79216d193f
ignite720 <ignite720@outlook.com>
2025-01-25 16:47:43 +0800

    init4

--- mathg.h
+++ mathg.h
@@ -5,17 +5,19 @@
 #define GLM_FORCE_INLINE
 #define GLM_FORCE_RADIANS
 
-#ifdef MATHG_ENABLE_RIGHT_HANDED
+#if MATHG_ENABLE_RIGHT_HANDED
     #define GLM_FORCE_RIGHT_HANDED
 #else
     #define GLM_FORCE_LEFT_HANDED
 #endif
 
-#ifdef MATHG_ENABLE_SIMD
+#if MATHG_ENABLE_SIMD
     #define GLM_FORCE_INTRINSICS
 #endif
 
 //#define GLM_FORCE_CXX98
+//#define GLM_FORCE_CXX11
+//#define GLM_FORCE_CXX14
 //#define GLM_FORCE_CXX17
 //#define GLM_FORCE_MESSAGES
 #define GLM_ENABLE_EXPERIMENTAL
@@ -34,36 +36,63 @@
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>          // ext, gtc, gtx, etc.
 
-#define DEFINE_EXTRA_VECTOR_TYPES(prefix, prefix_og) \
-using Vector2##prefix = glm::##prefix_og##vec2; \
-using Vector3##prefix = glm::##prefix_og##vec3; \
-using Vector4##prefix = glm::##prefix_og##vec4;
+#define MATHG_DEFINE_TYPES(prefix, _comment) \
+using Vector1##prefix = glm::prefix##vec1; \
+using Vector2##prefix = glm::prefix##vec2; \
+using Vector3##prefix = glm::prefix##vec3; \
+using Vector4##prefix = glm::prefix##vec4; \
+using Matrix2x2##prefix = glm::prefix##mat2x2; \
+using Matrix2x3##prefix = glm::prefix##mat2x3; \
+using Matrix2x4##prefix = glm::prefix##mat2x4; \
+using Matrix3x2##prefix = glm::prefix##mat3x2; \
+using Matrix3x3##prefix = glm::prefix##mat3x3; \
+using Matrix3x4##prefix = glm::prefix##mat3x4; \
+using Matrix4x2##prefix = glm::prefix##mat4x2; \
+using Matrix4x3##prefix = glm::prefix##mat4x3; \
+using Matrix4x4##prefix = glm::prefix##mat4x4;
 
-#define DEFINE_EXTRA_MATRIX_TYPES(prefix, prefix_og) \
-using prefix##2 = glm::##prefix_og##vec2; \
-using prefix##2x2 = glm::##prefix_og##mat2x2; \
-using prefix##2x3 = glm::##prefix_og##mat2x3; \
-using prefix##2x4 = glm::##prefix_og##mat2x4; \
-using prefix##3 = glm::##prefix_og##vec3; \
-using prefix##3x2 = glm::##prefix_og##mat3x2; \
-using prefix##3x3 = glm::##prefix_og##mat3x3; \
-using prefix##3x4 = glm::##prefix_og##mat3x4; \
-using prefix##4 = glm::##prefix_og##vec4; \
-using prefix##4x2 = glm::##prefix_og##mat4x2; \
-using prefix##4x3 = glm::##prefix_og##mat4x3; \
-using prefix##4x4 = glm::##prefix_og##mat4x4;
+#define MATHG_DEFINE_TYPES_HLSL(suffix, prefix_og) \
+using Vector1##suffix = glm::prefix_og##1; \
+using Vector2##suffix = glm::prefix_og##2; \
+using Vector3##suffix = glm::prefix_og##3; \
+using Vector4##suffix = glm::prefix_og##4; \
+using Matrix2x2##suffix = glm::prefix_og##2x2; \
+using Matrix2x3##suffix = glm::prefix_og##2x3; \
+using Matrix2x4##suffix = glm::prefix_og##2x4; \
+using Matrix3x2##suffix = glm::prefix_og##3x2; \
+using Matrix3x3##suffix = glm::prefix_og##3x3; \
+using Matrix3x4##suffix = glm::prefix_og##3x4; \
+using Matrix4x2##suffix = glm::prefix_og##4x2; \
+using Matrix4x3##suffix = glm::prefix_og##4x3; \
+using Matrix4x4##suffix = glm::prefix_og##4x4;
 
-DEFINE_EXTRA_VECTOR_TYPES(, )
-DEFINE_EXTRA_VECTOR_TYPES(b, b)
-DEFINE_EXTRA_VECTOR_TYPES(i, i)
-DEFINE_EXTRA_VECTOR_TYPES(u, u)
-DEFINE_EXTRA_VECTOR_TYPES(u8, u8)
+MATHG_DEFINE_TYPES(i8, "i8")
+MATHG_DEFINE_TYPES(u8, "u8")
+MATHG_DEFINE_TYPES(u16, "half")
+MATHG_DEFINE_TYPES(u, "uint")
 
-DEFINE_EXTRA_MATRIX_TYPES(Float, )
-DEFINE_EXTRA_MATRIX_TYPES(Sint, i)
-DEFINE_EXTRA_MATRIX_TYPES(Uint, u)
+MATHG_DEFINE_TYPES_HLSL(b, bool)
+MATHG_DEFINE_TYPES_HLSL(i, int)
+//MATHG_DEFINE_TYPES_HLSL(u16, half)
+MATHG_DEFINE_TYPES_HLSL(f, float)
+MATHG_DEFINE_TYPES_HLSL(d, double)
 
-using Matrix4x4 = Float4x4;
-using Quaternion = glm::quat;
+#if 0
+using Vector2b = glm::bool2;
+using Vector3b = glm::bvec3;
+using Vector4b = glm::bvec4;
+using Matrix2x2b = glm::bool2x2;
+using Matrix2x3b = glm::bool2x3;
+using Matrix2x4b = glm::bool2x4;
+using Matrix3x2b = glm::bool3x2;
+using Matrix3x3b = glm::bool3x3;
+using Matrix3x4b = glm::bool3x4;
+using Matrix4x2b = glm::bool4x2;
+using Matrix4x3b = glm::bool4x3;
+using Matrix4x4b = glm::bool4x4;
+#endif
+
+using Quaternionf = glm::quat;
+using Quaterniond = glm::dquat;
 
 namespace mg = glm;
