56bce39eb7605bd7e105ae5d76b4bf6e88e65e63
ignite720 <ignite720@outlook.com>
2025-11-23 16:02:31 +0800

    init10

--- mathx_vk.h
+++ mathx_vk.h
@@ -1,51 +1,47 @@
 #ifndef __MATHX_VK_H__
 #define __MATHX_VK_H__
 
-#define MATHX_FLOAT                                             float
-#define MATHX_UINT64                                            uint64_t
+#define MATHX_VK_BOOL                                           MATHX_VK_UINT
+#define MATHX_VK_FLOAT                                          float
+#define MATHX_VK_UINT64                                         uint64_t
 
 #ifdef __cplusplus
 
 #include <cstdint>
 
-#define MATHX_VEC2                                              Vector2f
-#define MATHX_VEC3                                              Vector3f
-#define MATHX_VEC4                                              Vector4f
+#define MATHX_VK_VEC2                                           Vector2f
+#define MATHX_VK_VEC3                                           Vector3f
+#define MATHX_VK_VEC4                                           Vector4f
 
-#define MATHX_INT                                               int32_t
-#define MATHX_IVEC2                                             Vector2i
-#define MATHX_IVEC3                                             Vector3i
-#define MATHX_IVEC4                                             Vector4i
+#define MATHX_VK_INT                                            int32_t
+#define MATHX_VK_IVEC2                                          Vector2i
+#define MATHX_VK_IVEC3                                          Vector3i
+#define MATHX_VK_IVEC4                                          Vector4i
 
-#define MATHX_UINT32                                            uint32_t
-#define MATHX_BOOL32                                            uint32_t
+#define MATHX_VK_UINT                                           uint32_t
+#define MATHX_VK_UVEC2                                          Vector2u
+#define MATHX_VK_UVEC3                                          Vector3u
+#define MATHX_VK_UVEC4                                          Vector4u
 
-#define MATHX_UVEC2                                             Vector2u
-#define MATHX_UVEC3                                             Vector3u
-#define MATHX_UVEC4                                             Vector4u
-
-#define MATHX_MAT4                                              Matrix4x4f
+#define MATHX_VK_MAT4                                           Matrix4x4f
 
 #else
 
-#define MATHX_FLOAT                                             float
-#define MATHX_VEC2                                              vec2
-#define MATHX_VEC3                                              vec3
-#define MATHX_VEC4                                              vec4
+#define MATHX_VK_VEC2                                           vec2
+#define MATHX_VK_VEC3                                           vec3
+#define MATHX_VK_VEC4                                           vec4
 
-#define MATHX_INT                                               int
-#define MATHX_IVEC2                                             ivec2
-#define MATHX_IVEC3                                             ivec3
-#define MATHX_IVEC4                                             ivec4
+#define MATHX_VK_INT                                            int
+#define MATHX_VK_IVEC2                                          ivec2
+#define MATHX_VK_IVEC3                                          ivec3
+#define MATHX_VK_IVEC4                                          ivec4
 
-#define MATHX_UINT32                                            uint
-#define MATHX_BOOL32                                            uint
+#define MATHX_VK_UINT                                           uint
+#define MATHX_VK_UVEC2                                          uvec2
+#define MATHX_VK_UVEC3                                          uvec3
+#define MATHX_VK_UVEC4                                          uvec4
 
-#define MATHX_UVEC2                                             uvec2
-#define MATHX_UVEC3                                             uvec3
-#define MATHX_UVEC4                                             uvec4
-
-#define MATHX_MAT4                                              mat4
+#define MATHX_VK_MAT4                                           mat4
 
 #define printf                                                  debugPrintfEXT
 
@@ -58,7 +54,7 @@
 #extension GL_EXT_shader_explicit_arithmetic_types_int64 : require
 #extension GL_EXT_shader_image_load_formatted : require
 
-#ifdef MATHX_ENABLE_RAYTRACING
+#ifdef MATHX_VK_ENABLE_RAYTRACING
 #extension GL_EXT_ray_query : require
 #extension GL_EXT_ray_tracing : require
 #extension GL_EXT_ray_tracing_position_fetch : require
